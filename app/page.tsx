163ef9991177d7814f82be5fb5b59259b9673f18
Nikolas Rummel <nikolasrummel6@gmail.com>
2023-11-29 23:49:17 +0100

    added image to culinarium widget

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import ThemeChanger from "@/components/theme-toggle";
 import Widget from "@/components/widget";
 import UserWidget from "@/components/user-widget";
 import Example from "@/components/charts/culinarium-chart";
+import culinarium from "@/public/images/culinarium.png";
 
 export default async function Home() {
 
@@ -18,12 +19,9 @@ export default async function Home() {
                 <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-40"}>
                     <UserWidget image={"https://nikolas-rummel.de/images/me.png"} name={"Nikolas"} email={"privat@nikolas-rummel.de"}/>
                     <Widget title={"Calendar"} description={"Mi. 29. Nov 2023"} colSpan={2}>
-                        <p>d</p>
+                        <p className={"py-20"}>no meetings for today</p>
                     </Widget>
-                    <Widget title={"Calendar"} description={"Mi. 29. Nov 2023"} colSpan={2}>
-                        <p>d</p>
-                    </Widget>
-                    <Widget title={"Calendar"} description={"Mi. 29. Nov 2023"} colSpan={1}>
+                    <Widget title={"Culinarium"} description={"Current capacity"} colSpan={1} image={culinarium}>
                         <div className={"relative"}>
                             <Example/>
                         </div>
--- components/widget.tsx
+++ components/widget.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import Image from "next/image";
+import Image, {StaticImageData} from "next/image";
 import calendar from "@/public/images/calendar.png";
-
 
 interface WidgetProps {
     title: string;
     description: string;
     children: React.ReactNode;
     colSpan?: number | 1;
+    image?: StaticImageData;
 }
 
-const Widget = ({title, description, children, colSpan}: WidgetProps) => {
+const Widget = ({title, description, children, colSpan, image}: WidgetProps) => {
     return (
         <div
             className={`shadow-xl hover:shadow-2xl hover:scale-[1.02] duration-300 rounded-b-3xl col-span-${colSpan}`}>
             <div className={"rounded-t-3xl bg-sky-100 dark:bg-neutral-950 p-3 w-full inline-flex space-x-4"}>
-                <Image src={calendar} alt={"calendar"} className={"rounded-2xl h-12 w-12"}/>
+                <Image src={image? image : calendar} alt={"calendar"} className={"rounded-2xl h-12 w-12"}/>
                 <div className={"col-span-4"}>
                     <span className={"block text-xl font-medium"}>{title}</span>
                     <span>{description}</span>
