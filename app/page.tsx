d498108e1df694a731b30f252676ffcfa789027a
Nikolas Rummel <nikolasrummel6@gmail.com>
2023-11-30 23:09:36 +0100

    changed layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,23 @@ export default async function Home() {
     return (
         <section className="min-h-screen flex items-center justify-center px-20">
             <div className={"w-[1200px]"}>
-                <div className={"flex flex-col"}>
-                    <h1 className={"text-4xl font-semibold"}>Cubid lifetracker</h1>
-                    <h2 className={"text-2xl "}>Organice your life in a simple dashboard.</h2>
+                <div className="inline-flex items-center justify-center space-x-80 mr-2">
+                    <div className="flex flex-col">
+                        <h1 className="text-4xl font-semibold">Cubid lifetracker</h1>
+                        <h2 className="text-2xl">Organize your life in a simple dashboard.</h2>
+                    </div>
+                    <div className="">
+                        <SevenSegmentClock />
+                    </div>
                 </div>
 
                 <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-20 gap-10"}>
                     <UserWidget image={"https://nikolas-rummel.de/images/me.png"} name={"Nikolas"}
                                 email={"privat@nikolas-rummel.de"}/>
+                    <div className={"col-span-2"}>
+                        <WeatherWidget/>
+                    </div>
+
                     <div className={"h-full col-span-2"}>
                         <Widget title={"Calendar"} description={"Mi. 29. Nov 2023"} colSpan={2}>
                             <p className={"my-32"}>no meetings for today</p>
@@ -27,8 +36,6 @@ export default async function Home() {
                             <Example/>
                         </div>
                     </Widget>
-                    <WeatherWidget/>
-                    <SevenSegmentClock/>
                 </div>
             </div>
         </section>
